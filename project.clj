8d92d90ec374b41e63d257616235f51f9cff002a
solicode <solicode@gmail.com>
2015-10-28 13:03:49 +0900

    Upgrade ClojureScript version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :main nippongo.core
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48"]]
+                 [org.clojure/clojurescript "1.7.145"]]
   :aliases {"test-all" ["do" "clean," "test," "cljsbuild" "test"]}
   :plugins [[lein-cljsbuild "1.1.0"]]
   :cljsbuild {:builds        [{:id           "test"
